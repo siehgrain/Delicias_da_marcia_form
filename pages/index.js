a5274ddd97ba065a3f79d73b32d6159321491eca
Leonardo Gomes <leoonardo.g_m@hotmail.com>
2022-01-17 22:51:52 -0300

    correções e navbar

--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,23 @@ import { useState } from 'react';
 
 function Home() {
     return (
-        <div>
-            <label>Nome<input type="text" id="UserName" name="Name" /></label>
-            <label>Endereço<input type="text" id="User" name="Name" /></label>
-            <p></p>
-            <div className="radio-buttons">
-        <input
-          id="windows"
-          value="windows"
-          name="platform"
-          type="radio"
-        />Massa branca
-        </div>
-        <div className="radio-buttons">
-        <input
-          id="windows"
-          value="windows"
-          name="platform"
-          type="radio"
-        />Massa de Chocolate
-        </div>
-        <p></p>
-        <div class="container">
-            <h3>Kilos</h3>
-        </div>
-        <div class="stepper-input">
-      <button class="btn btn-left">-</button>
-      <input type="text" placeholder="2" class="input-box"/>
-      <button class="btn btn-right">+</button>
-   </div>
-        </div>
+        <div className='navbar' >
+          <div className='Links' >
+            <a href=''>Home</a>
+          <div className='Links'>
+            <a href=''>Galeria</a> 
+          </div>
+          <div className='Links'>
+            <a href=''>Sobre</a> 
+          </div>
+          <div className='Links'>
+            <a href=''>Contato</a> 
+          </div>
+          <div className='btn'>
+            <a className='s' href=''>Encomende Já</a> 
+          </div>
+          </div>       
+        </div>  
     )
     }
 export default Home
